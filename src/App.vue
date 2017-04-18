35aaaa0a5e33534c8095468ba5847d3c4b2da1f4
loosue <15870959327@163.com>
2017-04-18 15:57:02 +0800

    init vue-3 demo to repository

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+html,body,header,footer,h1,h2,p,ul,li,div{padding: 0;margin: 0;}
+h1, h2 {font-weight: normal;}
+ul {list-style-type: none;}
+li {display: inline-block;margin: 0 10px;}
+a {text-decoration: none;color: #42b983;}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+      <ul class="header-nav">
+        <li v-on:click = "navclick"><a href="#">Javascript</a></li>
+        <li ><a href="#">React</a></li>
+        <li ><a href="#">Java</a></li>
+        <li ><a href="#">vue</a></li>
+        <li ><a href="#">angular js</a></li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'header',
+  data: {
+    count: 0
+  },
+  methods: {
+    navclick: function(){
+      console.log(this);
+      data.count+=1
+      console.log(data.count);
+    }
+  }
+
+}
+</script>
+
+<style media="screen">
+.header{text-align: left;}
+  .header ul li{margin: 10px;text-align: center;}
+  .header-nav a{color: #868686;font-size: 20px;}
+  .header-nav a:hover{color: #101010;}
+</style>
