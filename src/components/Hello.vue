f365cd3e99548909e3ecbb9ae0529e8aeee77673
loosue <15870959327@163.com>
2017-04-18 17:42:56 +0800

    reset Hello.vue and add react.vue

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,17 +1,6 @@
 <template>
   <div class="hello">
     <my-header></my-header>
-    <img src="../assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
   </div>
 </template>
 
